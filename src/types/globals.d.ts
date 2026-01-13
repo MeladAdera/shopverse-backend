81423ef6677390ea5231f09402dea8f62f2a072e
Melad Adera <melad@example.com>
2026-01-13 14:13:00 +0300

    mmmkm

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -2,12 +2,13 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: 'development' | 'production' | 'test';
-      PORT: string;
-      DATABASE_URL: string;
-      JWT_SECRET: string;
-      JWT_EXPIRES_IN: string;
-      // أضف متغيرات بيئية أخرى هنا
+       NODE_ENV: 'development' | 'production' | 'test';
+    PORT: string;
+    HOST: string;
+    DATABASE_URL?: string;
+    JWT_SECRET: string;
+    JWT_REFRESH_SECRET: string;
+    FRONTEND_URL: string;
     }
   }
 }
